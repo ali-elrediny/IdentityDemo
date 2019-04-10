1696807e77958284d6109ddffa4715ada106ff03
Ali El-Rediny <ali.elrediny@yahoo.com>
2019-04-10 11:51:17 +0300

    Initial Commit

---
+++ IdenetityManagement/Model/ApplicationRole.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdenetityManagement.Model
+{
+    public class ApplicationRole : IdentityRole<Guid>
+    {
+        public string NameAr { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
---
+++ IdenetityManagement/Model/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdenetityManagement.Model
+{
+    public partial class ApplicationUser : IdentityUser<Guid>
+    {
+        [Required]
+        [StringLength(256)]
+        public string FullName { get; set; }
+
+        public bool IsActive { get; set; }
+
+
+    }
+}
+
