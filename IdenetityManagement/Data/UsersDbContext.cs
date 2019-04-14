b0a0e2aedb94f30555fd5d6386ecb812a48ac2dd
Yasser Sallam <yasserhuss44@gmail.com>
2019-04-14 01:43:19 -0700

    No Changes

---
+++ IdenetityManagement/Data/UsersDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IdenetityManagement.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdenetityManagement.Data
+{
+    public class UsersDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+    {
+
+        public UsersDbContext(DbContextOptions<UsersDbContext> options)
+            : base(options)
+        {
+        }
+
+    }
+}
---
+++ IdenetityManagement/Model/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Framework.Identity.Model
+{
+    public static class IdentityEnums
+    {
+        public enum AppRoles
+        {
+            Admin,
+            PowerUser,
+            HeadOfDelegates
+        }
+    }
+}
